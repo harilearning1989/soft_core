2e30ee20577a6410bd4dcb23526a2b8597ac1e3e
harilearning1989 <59171886+harilearning1989@users.noreply.github.com>
2025-04-15 16:50:52 +0530

    initial commit

---
+++ soft_core_service/src/main/java/com/web/soft/SoftCoreApplication.java
@@ -0,0 +1,13 @@
+package com.web.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftCoreApplication.class, args);
+	}
+
+}
---
+++ soft_core_service/src/test/java/com/web/soft/SoftCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.soft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
